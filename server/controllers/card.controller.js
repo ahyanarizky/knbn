51f1ce7874217455c465ec5c74c0a9e614d713e3
ryan <ryan.andhaka@gmail.com>
2016-11-11 13:00:16 +0700

    add controller

---
+++ server/controllers/card.controller.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const Card = require('../models/card.api.model')
+
+let showAllCard = (req, res) => {
+  Card
+    .find({}, (err, all_card) => {
+      if(err) res.status(400).json({'error': 'Error: ${err}'})
+      if(!all_card) res.status(404).json({'message':'Failed to load all cards'})
+
+      res.status(200).json(all_card)
+    })
+}
+
+let createCard = (req, res) => {
+  Card.create({
+    title: req.body.title
+    content: req.body.content
+    due_date: req.body.due_date
+    status: req.body.status
+    in_charge: req.body.in_charge
+  },
+  (err, new_card) => {
+    console.log(new_card);
+    if(err) res.status(400).json({'error': 'Error: ${err}'})
+    if(!new_card) res.status(404).json({'message':'Error to create new card'})
+
+    res.status(200).json(new_card)
+  })
+}
+
+let showCard = (req, res) =>{
+  cardID: req.params.cardID
+},
+(err, show_a_card) => {
+  console.log(show_a_card);
+  if(err) res.status(400).json({'error': 'Error: ${err}'})
+  if(!show_a_card) res.status(404).json({'message':'Error to show a card'})
+
+  res.status(200).json(show_a_card)
+}
+
+let editCard = (req, res) => {
+  Card.findOneAndUpdate({
+    cardID: req.params.cardID
+  }, req.body,{
+    new: true
+  },(err, edited_card) => {
+      if(err) res.status(400).json({'Error: ${err}'})
+      if(!edited_card) res.status(404).json({'message':'Error to edit a card'})
+
+      res.status(200).json(edited_card)
+  })
+}
+
+let deleteCard = (req, res) => {
+  Card.findOneandRemove({
+    cardID: req.params.cardID
+  }, (err, delete_card) => {
+        if(err) res.status(400).json({'Error: ${err}'})
+        if(!delete_card) res.status(404).json({'message':'Error to delete a card'})
+
+        res.status(200).json(delete_card)
+  })
+}
+
+module.exports = {
+  showAllCard,
+  createCard,
+  showCard,
+  editCard,
+  deleteCard
+}
