44c6c2a14f47cc7b03195b69df97e943ef3e7208
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-11 14:14:40 +0700

    add new package for incrementing

--- server/models/card.model.js
+++ server/models/card.model.js
@@ -1,15 +1,18 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence');
 const Schema = mongoose.Schema
 
-const cardSchema = {
+const cardSchema = new Schema({
     cardID: { type: Number, unique: true },
     title: { type: String, required: true },
     content: { type: String, required: true },
     due_date: { type: Date, required: true },
     status: { type: String, required: true },
     in_charge: { type: String, required: true }
-}
+})
+
+cardSchema.plugin(AutoIncrement, { inc_field: 'cardID' });
 
 module.exports = mongoose.model('Card', cardSchema)
