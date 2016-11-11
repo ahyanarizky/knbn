084d5ba5146a6104d801b3795a29e2f4bd23df3c
Septian A Tama <sfirst.tama@gmail.com>
2016-11-11 17:31:08 +0700

    Dengan mengucap syukur kepada Allah SWT, saya menyatakan knbn_v0.5 release

--- client/javascripts/script.js
+++ client/javascripts/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 })
 
 //NEW DATA ITEM
@@ -67,28 +66,28 @@ function createNewCard() {
                 </div>
                 <div class="modal-body">
                   <!-- INPUT FORM -->
-                  <form id="addCard">
+                  <form id="editCard">
                     <div class="form-group">
                       <label class="control-label" for="cardTitle">Title</label>
-                      <input class="form-control" id="cardTitle" type="text" placeholder="Enter the thing you wanna do . ." value="${data.title}">
+                      <input class="form-control" id="cardTitle${data.cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data.title}">
                     </div>
                     <div class="form-group">
                       <label class="control-label" for="cardDesc">Description</label>
-                      <textarea class="form-control" rows="3" id="cardDesc" placeholder="Describe the thing you wanna do . .">${data.content}</textarea>
+                      <textarea class="form-control" rows="3" id="cardDesc${data.cardID}" placeholder="Describe the thing you wanna do . .">${data.content}</textarea>
                     </div>
                     <div class="form-group">
                       <label class="control-label" for="cardInCharge">In Charge</label>
-                      <input class="form-control" id="cardInCharge" type="text" placeholder="Enter the thing you wanna do . ." value="${data.in_charge}">
+                      <input class="form-control" id="cardInCharge${data.cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data.in_charge}">
                     </div>
                     <div class="form-group">
                       <label class="control-label" for="cardDueDate">Due Date</label>
-                      <input class="form-control" id="cardDueDate" type="text" placeholder="Enter the thing you wanna do . ." value="${data.due_date}">
+                      <input class="form-control" id="cardDueDate${data.cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data.due_date}">
                     </div>
                   </form>
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-success" onclick="updateCard('${data.cardID}')">Save changes</button>
+                  <button type="button" class="btn btn-success" onclick="updateCard('${data.cardID}')" data-dismiss="modal">Save changes</button>
                 </div>
               </div>
             </div>
@@ -288,7 +287,235 @@ function todo(id) {
 }
 
 function updateCard(id) {
+  $.ajax({
+    url: "http://localhost:3000/api/cards/"+id,
+    method: 'PUT',
+    contentType: 'application/x-www-form-urlencoded',
+    data: {
+      title: $(`#cardTitle${id}`).val(),
+      content: $(`#cardDesc${id}`).val(),
+      due_date: $(`#cardDueDate${id}`).val(),
+      in_charge: $(`#cardInCharge${id}`).val()
+    },
+    success: function(data) {
+      console.log(data);
+      var newtitle = `<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne${data.cardID}" aria-expanded="true" aria-controls="collapseOne" id="currentTitle${data.cardID}">
+        ${data.title}
+      </a>`;
+      var newcontent = `<p class="list-group-item-text text-left" id="currentContent${data.cardID}">${data.content}</p>`;
+      var newdue_date = `<p class="list-group-item-text text-left" id="currentDueDate${data.cardID}">${data.due_date}</p>`;
+      var newin_charge = `<span id="currentInCharge${data.cardID}">${data.in_charge}</span>`;
+      $(`#currentTitle${id}`).replaceWith(newtitle);
+      $(`#currentContent${id}`).replaceWith(newcontent);
+      $(`#currentInCharge${id}`).replaceWith(newin_charge);
+      $(`#currentDueDate${id}`).replaceWith(newdue_date);
+    }
+  })
+}
 
+function getAll() {
+  //GET ALL DATA ITEM
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/api/cards",
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded',
+    success: function(data) {
+      var todo ='';
+      var doing = '';
+      var done = '';
+      var modaldelete = '';
+      var modaledit = '';
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].status == 'todo') {
+          todo += `
+          <!-- Single Card -->
+          <div class="panel panel-default" id="card_${data[i].cardID}">
+            <div class="panel-heading" role="tab" id="headingOne">
+              <h4 class="panel-title">
+                <button type="button" class="close" data-toggle="modal" data-target="#modalDelete_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-trash"></span></button>
+
+                <button type="button" class="close" data-toggle="modal" data-target="#modalEdit_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-edit"></span></button>
+
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne${data[i].cardID}" aria-expanded="true" aria-controls="collapseOne" id="currentTitle${data[i].cardID}">
+                  ${data[i].title}
+                </a>
+              </h4>
+            </div>
+            <div class="panel-body">
+              <div class="alert alert-dismissible alert-info">
+                <strong>In Charge : </strong> <span id="currentInCharge${data[i].cardID}">${data[i].in_charge}</span>
+              </div>
+            </div>
+            <div id="collapseOne${data[i].cardID}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="list-group">
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Description</h4>
+                    <p class="list-group-item-text text-left" id="currentContent${data[i].cardID}">${data[i].content}</p>
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Due Date</h4>
+                    <p class="list-group-item-text text-left" id="currentDueDate${data[i].cardID}">${data[i].due_date}</p>
+                  </a>
+                  <div class="list-group-item">
+                    <ul class="pager">
+                      <li class="next"><a href="#" onclick="doing('${data[i].cardID}')">Doing &rarr;</a></li>
+                    </ul>
+                  </div>
+
+                </div>
+            </div>
+          </div>
+          <!-- Single Card -->`;
+        } else if(data[i].status == 'doing') {
+          doing += `<!-- Single Card -->
+          <div class="panel panel-default" id="card_${data[i].cardID}">
+            <div class="panel-heading" role="tab" id="headingOne">
+              <h4 class="panel-title">
+                <button type="button" class="close" data-toggle="modal" data-target="#modalDelete_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-trash"></span></button>
+
+                <button type="button" class="close" data-toggle="modal" data-target="#modalEdit_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-edit"></span></button>
+
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne${data[i].cardID}" aria-expanded="true" aria-controls="collapseOne" id="currentTitle${data[i].cardID}">
+                  ${data[i].title}
+                </a>
+              </h4>
+            </div>
+            <div class="panel-body">
+              <div class="alert alert-dismissible alert-info">
+                <strong>In Charge : </strong> <span id="currentInCharge${data[i].cardID}">${data[i].in_charge}</span>
+              </div>
+            </div>
+            <div id="collapseOne${data[i].cardID}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="list-group">
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Description</h4>
+                    <p class="list-group-item-text text-left" id="currentContent${data[i].cardID}">${data[i].content}</p>
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Due Date</h4>
+                    <p class="list-group-item-text text-left" id="currentDueDate${data[i].cardID}">${data[i].due_date}</p>
+                  </a>
+                  <div class="list-group-item">
+                    <ul class="pager">
+                      <li class="previous" onclick="todo('${data[i].cardID}')""><a href="#">&larr; To Do</a></li>
+                      <li class="next"><a href="#" onclick="done('${data[i].cardID}')">Done &rarr;</a></li>
+                    </ul>
+                  </div>
+
+                </div>
+            </div>
+          </div>
+          <!-- Single Card -->`;
+        } else {
+          done += `<!-- Single Card -->
+          <div class="panel panel-default" id="card_${data[i].cardID}">
+            <div class="panel-heading" role="tab" id="headingOne">
+              <h4 class="panel-title">
+                <button type="button" class="close" data-toggle="modal" data-target="#modalDelete_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-trash"></span></button>
+
+                <button type="button" class="close" data-toggle="modal" data-target="#modalEdit_${data[i].cardID}"><span aria-hidden="true" class="glyphicon glyphicon-edit"></span></button>
+
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne${data[i].cardID}" aria-expanded="true" aria-controls="collapseOne" id="currentTitle${data[i].cardID}">
+                  ${data[i].title}
+                </a>
+              </h4>
+            </div>
+            <div class="panel-body">
+              <div class="alert alert-dismissible alert-info">
+                <strong>In Charge : </strong> <span id="currentInCharge${data[i].cardID}">${data[i].in_charge}</span>
+              </div>
+            </div>
+            <div id="collapseOne${data[i].cardID}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="list-group">
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Description</h4>
+                    <p class="list-group-item-text text-left" id="currentContent${data[i].cardID}">${data[i].content}</p>
+                  </a>
+                  <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Due Date</h4>
+                    <p class="list-group-item-text text-left" id="currentDueDate${data[i].cardID}">${data[i].due_date}</p>
+                  </a>
+                  <div class="list-group-item">
+                    <ul class="pager">
+                      <li class="previous"><a href="#" onclick="doing('${data[i].cardID}')">&larr; Doing</a></li>
+                    </ul>
+                  </div>
+
+                </div>
+            </div>
+          </div>
+          <!-- Single Card -->`;
+        }
+
+       modaledit += `<!-- MODAL EDIT -->
+           <div class="modal fade" id="modalEdit_${data[i].cardID}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+             <div class="modal-dialog" role="document">
+               <div class="modal-content">
+                 <div class="modal-header">
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                   <h4 class="modal-title" id="myModalLabel">Edit ${data[i].title}</h4>
+                 </div>
+                 <div class="modal-body">
+                   <!-- INPUT FORM -->
+                   <form id="editCard">
+                     <div class="form-group">
+                       <label class="control-label" for="cardTitle">Title</label>
+                       <input class="form-control" id="cardTitle${data[i].cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data[i].title}">
+                     </div>
+                     <div class="form-group">
+                       <label class="control-label" for="cardDesc">Description</label>
+                       <textarea class="form-control" rows="3" id="cardDesc${data[i].cardID}" placeholder="Describe the thing you wanna do . .">${data[i].content}</textarea>
+                     </div>
+                     <div class="form-group">
+                       <label class="control-label" for="cardInCharge">In Charge</label>
+                       <input class="form-control" id="cardInCharge${data[i].cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data[i].in_charge}">
+                     </div>
+                     <div class="form-group">
+                       <label class="control-label" for="cardDueDate">Due Date</label>
+                       <input class="form-control" id="cardDueDate${data[i].cardID}" type="text" placeholder="Enter the thing you wanna do . ." value="${data[i].due_date}">
+                     </div>
+                   </form>
+                 </div>
+                 <div class="modal-footer">
+                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                   <button type="button" class="btn btn-success" onclick="updateCard('${data[i].cardID}')" data-dismiss="modal">Save changes</button>
+                 </div>
+               </div>
+             </div>
+           </div>`;
+
+       modaldelete += `<!-- MODAL DELETE -->
+       <div class="modal fade" id="modalDelete_${data[i].cardID}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+         <div class="modal-dialog" role="document">
+           <div class="modal-content">
+             <div class="modal-header">
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+               <h4 class="modal-title" id="myModalLabel">Delete ${data[i].title}</h4>
+             </div>
+             <div class="modal-body">
+               Are you sure, want to delete this?
+             </div>
+             <div class="modal-footer">
+               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+               <button type="button" class="btn btn-danger" onclick="deleteCard('${data[i].cardID}')" data-dismiss="modal">Delete</button>
+             </div>
+           </div>
+         </div>
+       </div>
+       `;
+
+      }
+      $(".todo").append(todo);
+      $(".doing").append(doing);
+      $(".done").append(done);
+      $("body").append(modaledit);
+      $("body").append(modaldelete);
+
+    }
+
+
+  })
 
 }
 
@@ -311,4 +538,5 @@ function deleteCard(id) {
 function showmain() {
   $('#intro').hide();
   $('#main').show();
+  getAll();
 }
